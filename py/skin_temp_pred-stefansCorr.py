698df79503e530b623119897279233d97f5c744b
Veethahavya <veethahavya@gmail.com>
2020-08-05 13:36:22 +0200

    Major update: with corrections from Stefan, the model runs and shows the diurinal cyclic nature as expected. Unexplained bugs: - Ts is on the negative side while it should be +ve as it is the summer. - The scale of Ts is too small

--- py/skin_temp_pred-stefansCorr.py
+++ py/skin_temp_pred-stefansCorr.py
@@ -18,10 +18,11 @@ LW = dfLW['LW'].tolist()
 dfSW = pd.read_csv('..\data\shortwave_rad.dat', sep="\s+", names=['Date','Time', 'SW'])
 SW = dfSW['SW'].tolist()
 
-dfuwstr = pd.read_csv(r'..\data\ustar.dat', sep="\s+", names=['Date','Time', 'uw_str'])
-uw_str = dfuwstr['uw_str'].tolist()
+dfUwstr = pd.read_csv(r'..\data\ustar.dat', sep="\s+", names=['Date','Time', 'Uw_str'])
+Uw_str = dfUwstr['Uw_str'].tolist()
 
 time_check = dfLH['Time'].all() == dfSH['Time'].all() == dfLW['Time'].all()
+
 
 #%% ODE function definition
 #Interpolation function for smaller dt chosen by the solver
@@ -29,12 +30,13 @@ def interpolate(lst,t):
     value = 0
     if t == 0:
         value = lst[0]
-    if t == len(lst) or t > len(lst):
-        value = lst[len(lst)]
+    elif t >= len(lst):
+        value = lst[len(lst)-1]
     else:
-        np.interp(t, list(range(0,len(lst))), lst)
+        value = np.interp(t, list(range(0,len(lst))), lst)
     return value
 
+#ODE function
 def func(T, t):
     lh = interpolate(LH, t)
     sh = interpolate(SH, t)
@@ -50,7 +52,7 @@ def func(T, t):
 
     ro_w = 1000
     cw = 4174
-    v = 0.3
+    v = 1
 
     x = (Q + Rs - R_d)/(d*ro_w*cw*v/(v+1))
 
@@ -58,38 +60,35 @@ def func(T, t):
     k = 0.4
     alpha_w = 210e-6 #assumed thermal expansion coeff of water at 20C
     g = 9.8
-    uw = interpolate(uw_str, t)
+    Uw = interpolate(Uw_str, t)
 
-    if T > 0:
-        Fd = ((v*g*alpha_w/(5*d))**0.5)*ro_w*cw*(uw**2)*(T**0.5)
-    else:
-        Fd = g * alpha_w * (Q + Rs - R_d)
+    if T > 0: Fd = ((v*g*alpha_w/(5*d))**0.5)*ro_w*cw*(Uw**2)*(T**0.5)
+    else:     Fd = g * alpha_w * (Q + Rs - R_d)
 
-    L = ro_w*cw*uw**3/(k*Fd)
+    L = ro_w*cw*Uw**3/(k*Fd)
 
     if L <= 0:   phi_t_d_L = 1 + 5 * (-d / L)
     else:        phi_t_d_L = (1 - 16 * (-d / L))**-0.5
 
-    c = ((v+1)*k*uw)/(d*phi_t_d_L)
+    c = ((v+1)*k*Uw)/(d*phi_t_d_L)
 
 
     dTdt = x - c*T
     return dTdt
 
 #%% ODE Solver Init and Run
-y0 = [0.001]
-
+y0 = [0.00001]
 t = []
 if time_check: t = list(range(len(LH)))
-
 T = integrate.odeint(func, y0, t)
 
 #%%Post-processing
+
 #Save DF output as .dat
 dfT = pd.DataFrame(T, columns=['dTdt'])
 dfT['Datetime'] = pd.to_datetime(dfLH['Date'] + ' ' + dfLH['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
 dfT = dfT.set_index('Datetime')
-dat_path = r'..\results\dTdt_' + time.strftime("%Y%m%d%H%M%S") + '_y0-0.01' + '.dat'
+dat_path = r'..\results\dTdt_' + time.strftime("%Y%m%d%H%M%S") + '.dat'
 dfT.to_csv(dat_path, sep = ' ')
 
 #Plot and save figure
@@ -98,9 +97,21 @@ plt.figure(figsize=(20,6), dpi=120)
 ax = plt.axes()
 ax.xaxis.set_major_locator(plt.MaxNLocator(8))
 dfT.plot(ax=ax)
-png_path = r'..\results\dTdt_' + time.strftime("%Y%m%d%H%M%S") + '_y0-0.01' + '.png'
+png_path = r'..\results\dTdt_' + time.strftime("%Y%m%d%H%M%S") + '.png'
 plt.savefig(png_path)
+
+#Change index of input DFs to datetime
+dfLH['Datetime'] = pd.to_datetime(dfLH['Date'] + ' ' + dfLH['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
+dfLH = dfLH.set_index('Datetime')
+dfSH['Datetime'] = pd.to_datetime(dfSH['Date'] + ' ' + dfSH['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
+dfSH = dfSH.set_index('Datetime')
+dfLW['Datetime'] = pd.to_datetime(dfLW['Date'] + ' ' + dfLW['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
+dfLW = dfLW.set_index('Datetime')
+dfSW['Datetime'] = pd.to_datetime(dfSW['Date'] + ' ' + dfSW['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
+dfSW = dfSW.set_index('Datetime')
+dfUwstr['Datetime'] = pd.to_datetime(dfUwstr['Date'] + ' ' + dfUwstr['Time'], format='%d%m%Y %H:%M:%S', errors='ignore')
+dfUwstr = dfUwstr.set_index('Datetime')
 
 
 #%% Clearing unwanted variables
-del T, ax, dat_path, png_path, t, y0, uw_str, dfLH, dfLW, dfSH, dfSW, dfuwstr
+del T, ax, dat_path, png_path, t, y0, Uw_str, #dfLH, dfLW, dfSH, dfSW, dfUwstr
